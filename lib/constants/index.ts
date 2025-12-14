9bb43357cbe295c676d440d12daa882700b44909
entoun8 <entounsaid@gmail.com>
2025-12-14 18:16:31 +1100

    Working on layout

---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = "Alshami";
+export const APP_DESCRIPTION =
+  "A modern e-commerce platform for premium herbs, coffees, and more.";
+export const SERVER_URL = "http://localhost:3000";
